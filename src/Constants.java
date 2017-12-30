e894d32c1a917618e368ee283ffe8ba4565de380
nansyg21 <it12148@uom.edu.gr>
2017-12-30 22:21:12 +0200

    Insert DB connector

---
+++ src/Constants.java
@@ -0,0 +1,8 @@
+
+public class Constants {
+
+	//phpMyAdmin credentials for DB connection
+	public static String dbConnectionString = "jdbc:mysql://83.212.105.45:3306/stockmarket";
+	public static String dbUsername = "mai_user";
+	public static String dbPassword = "mai_user201";
+}
